885c79415c031c6bdeb2301e73c3ec75eb88f63d
voidk <voidk@asus.lan>
2017-09-14 01:20:49 +0200

    Replace old test

--- src/__test__/index.js
+++ src/__test__/index.js
@@ -1,17 +1,57 @@
 import should from 'should';
 import axios from 'axios';
-
 import config from '../../config';
 import run from '../run';
 
 const { describe, it } = global;
 
+const data = {
+  types: [
+    {
+      type: '1',
+      avatar: 'a',
+    },
+    {
+      type: '2',
+      avatar: 'b',
+    },
+  ],
+  dataflows: [
+    {
+      type: '1',
+    },
+    {
+      type: '1',
+    },
+  ],
+};
+
 describe('Pong test', () => {
   it('Should be equal to pong', (done) => {
-    run({ config }).then(() => axios.get('http://localhost:3004/ping'))
-      .then(({ data: { ping } }) => {
-        should(ping).eql('pong');
-        done();
-      }).catch(done);
+    run({ config })
+      .then(({ http }) => {
+         axios.get(`${http.url}/ping`)
+        .then(({ data: { ping } }) => {
+          should(ping).eql('pong');
+          done();
+          http.close();
+        })
+      })
+      .catch(done);
+  });
+});
+
+describe('groupBy test', () => {
+  it('Should have data grouped', (done) => {
+    run({ config, data })
+      .then(({ http }) => {
+        return axios.get(`${http.url}/groupBy`)
+        .then(({ data }) => {
+          should(data).eql({ a: 2 });
+          done();
+          http.close();
+        })
+      })
+      .catch(done);
   });
 });
